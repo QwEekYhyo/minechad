5e311569d93268cdad657dc849c679a2c53dfa18
Logan Lucas <logan.lucas83@gmail.com>
2024-10-17 18:13:59 +0200

    add serf spawn egg

--- src/main/java/com/qweekyhyo/minechad/item/ModItems.java
+++ src/main/java/com/qweekyhyo/minechad/item/ModItems.java
@@ -2,9 +2,11 @@ package com.qweekyhyo.minechad.item;
 
 import com.qweekyhyo.minechad.MineChad;
 import com.qweekyhyo.minechad.block.ModBlocks;
+import com.qweekyhyo.minechad.entity.ModEntities;
 import com.qweekyhyo.minechad.item.tool.Whip;
 import net.minecraft.item.BlockNamedItem;
 import net.minecraft.item.Item;
+import net.minecraftforge.common.ForgeSpawnEggItem;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -33,6 +35,16 @@ public class ModItems {
             "cotton",
             () -> new Item(new Item.Properties()
                     .tab(ModItemGroup.MINECHAD_GROUP)
+            )
+    );
+
+    public static final RegistryObject<Item> SERF_SPAWN_EGG = ITEMS.register(
+            "serf_spawn_egg",
+            () -> new ForgeSpawnEggItem(
+                    ModEntities.SERF,
+                    0x241511,
+                    0xF4BA74,
+                    new Item.Properties().tab(ModItemGroup.MINECHAD_GROUP)
             )
     );
 
